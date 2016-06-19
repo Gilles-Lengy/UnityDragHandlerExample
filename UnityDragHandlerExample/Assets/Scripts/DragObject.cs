40f27473b89674e3f9a0be98ead7fc8c63f166a0
Gilles Lengy <gilles.lengy@gmail.com>
2016-06-19 12:04:26 +0200

    Basic drag example. Watch the log to see something happened when dragging

---
+++ UnityDragHandlerExample/Assets/Scripts/DragObject.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;// Required when using Event data.
+
+public class DragObject : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    void Start()
+    {
+        Debug.Log("Start DragObject class !");
+    }
+
+    public void OnBeginDrag(PointerEventData _EventData)
+    {
+        Debug.Log("OnBeginDrag");
+    }
+
+    public void OnDrag(PointerEventData _EventData)
+    {
+        Debug.Log("OnDrag");
+
+    }
+
+    public void OnEndDrag(PointerEventData _EventData)
+    {
+        Debug.Log("OnEndDrag");
+    }
+}
