9a5ded350cebdba47e253ebf3226e30755f28c11
Gilles Lengy <gilles.lengy@gmail.com>
2016-07-25 21:40:02 +0200

    Unity crashed, I added the white square, the box collider and the DragMe script

---
+++ UnityDragHandlerExample/Assets/Scripts/SpawnBlackSquares.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnBlackSquares : MonoBehaviour {
+    public GameObject spriteToDuplicate;
+
+    // Use this for initialization
+    void Start () {
+        Vector3 currentPosition = spriteToDuplicate.transform.position;
+        for (int i = 0; i < 10; i++)
+        {
+            GameObject.Instantiate(spriteToDuplicate, currentPosition, Quaternion.identity);
+            currentPosition += new Vector3(1f, 0f, 0f);
+        }
+    }
+
+	
+
+}
