e4464d107b7056d51f4a803bc555bdede13a59e3
Gilles Lengy <gilles.lengy@gmail.com>
2016-07-25 21:49:26 +0200

    SpawnBlackSquares added even if we use it with a white square. the script is attache to the camera and the game object required is the square

--- UnityDragHandlerExample/Assets/Scripts/SpawnBlackSquares.cs
+++ UnityDragHandlerExample/Assets/Scripts/SpawnBlackSquares.cs
@@ -7,10 +7,10 @@ public class SpawnBlackSquares : MonoBehaviour {
     // Use this for initialization
     void Start () {
         Vector3 currentPosition = spriteToDuplicate.transform.position;
-        for (int i = 0; i < 10; i++)
+        for (int i = 0; i < 5; i++)
         {
             GameObject.Instantiate(spriteToDuplicate, currentPosition, Quaternion.identity);
-            currentPosition += new Vector3(1f, 0f, 0f);
+            currentPosition += new Vector3(3f, 0f, 0f);
         }
     }
 
